7143bbb1dfe446c6624ed022f8b60019c4b2f42d
Douglasjose3 <douglasjose3@gmail.com>
2024-02-13 23:57:39 -0800

    deploy das seções está resolvido, só fata a imagem do hero

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,30 @@
-console.log("Olá");
+AOS.init();
+
+const dataDoEvento = new Date("March 27, 2024 14:30:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEveneto = Math.floor((distanciaAteOEvento % horasAteOEvento) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEveneto}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `A fera chegou!`;
+    }
+}, 1000);
+
+
+
